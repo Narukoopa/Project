88ccc1d24c07546516a5f0f684a7ea3181e1d829
Josh Woodward <58075821+JWoodward230501@users.noreply.github.com>
2019-12-13 12:44:33 +0000

    Ver 0.0.4

--- Eros/Assets/Cubes.cs
+++ Eros/Assets/Cubes.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Cubes : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject cubeObj;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("f"))
+        {
+            for(int x = 0; x < 10000; x++)
+            {
+                Instantiate(cubeObj, new Vector3(this.transform.position.x, this.transform.position.y + 0.5f, this.transform.position.z), Quaternion.identity);
+            }
+        }
     }
 }
