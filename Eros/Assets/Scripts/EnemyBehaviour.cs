232d7bd5e10b6d1c0bdb21e19130ed1293445b47
Josh Woodward <58075821+JWoodward230501@users.noreply.github.com>
2020-01-08 22:18:32 +0000

    0.0.6

--- Eros/Assets/Scripts/EnemyBehaviour.cs
+++ Eros/Assets/Scripts/EnemyBehaviour.cs
@@ -5,22 +5,37 @@ using UnityEngine.AI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-
+    //VARIABLE DECLARATION
     private GameObject player;
     private NavMeshAgent navAgent;
 
+    private Player playerScript;
+
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
-        navAgent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player"); //FIND OBJECT TAGGED PLAYER, SET "player" TO IT
+        navAgent = GetComponent<NavMeshAgent>(); //FIND THE "NavMeshAgent" COMPONENT ON THIS OBJECT
+        playerScript = player.GetComponent<Player>(); //FIND THE "Player" SCRIPT ON "player"
     }
 
     
     void Update()
     {
-        float distanceToPlayer = Vector3.Distance(player.transform.position, this.transform.position);
+        float distanceToPlayer = Vector3.Distance(player.transform.position, this.transform.position); //CALCULATE THE DISTANCE BETWEEN THE OBJECTS, STORE DISTANCE IN "distanceToPlayer"
 
         if(distanceToPlayer < 15)
-            navAgent.SetDestination(player.transform.position);
+            navAgent.SetDestination(player.transform.position); //SET THE AI DESTINATION TO THE PLAYER'S TRANSFORM
+
+        if(distanceToPlayer < 2)
+        {
+            StartCoroutine("DealDamage");
+        }
+    }
+
+    IEnumerator DealDamage()
+    {
+        //PERFORM A SWING ANIMATION HERE
+        playerScript.DamagePlayer(1);
+        yield return new WaitForSeconds(0.5f); //ENSURE THE ENEMY DOES NOT CONTSTANTLY SWING
     }
 }
--- Eros/Assets/Scripts/Player.cs
+++ Eros/Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //VARIABLE DECLARATION
+
     public bool isGrounded;
     public bool isSprinting;
     private bool jumpRequest;
@@ -27,15 +29,16 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
-        cam = Camera.main.transform;
-        Cursor.lockState = CursorLockMode.Locked;
+        cam = Camera.main.transform; //SET "cam" EQUAL TO THE TRANSFORM COMPONENT OF THE MAIN CAMERA
+        Cursor.lockState = CursorLockMode.Locked; //LOCK THE CURSOR IN THE CENTRE OF THE SCREEN
     }
 
     private void FixedUpdate()
     {
-        CalculateVelocity();
-        if (jumpRequest)
-            Jump();
+        CalculateVelocity(); //CALCULATE THE PLAYER'S MOVEMENT VELOCITY
+
+        if (jumpRequest) //CHECK IF THE PLAYER WANTS TO JUMP
+            Jump(); //RUN THE JUMP FUNCTION
 
         transform.Rotate(Vector3.up * mouseHorizontal); //PLAYER BODY TURN
         cam.Rotate(Vector3.right * -mouseVertical); //CAMERA PAN
@@ -44,42 +47,42 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        GetPlayerInputs();
+        GetPlayerInputs(); //CHECK FOR PLAYER INPUTS
     }
 
-    public void DamagePlayer(float value)
+    public void DamagePlayer(float value) //ACCESSED BY ENEMY SCRIPTS
     {
-        health = (health - value);
+        health = (health - value); //TAKE OFF THE REQUESTED AMOUNT OF HEALTH
     }
 
     void Jump()
     {
-        verticalMomentum = jumpForce;
-        isGrounded = false;
+        verticalMomentum = jumpForce; //CHANGE THE PLAYER'S verticalMomentum EQUAL TO THE jumpForce
+        isGrounded = false; //SET THESE CHECK BOOLEANS TO FALSE
         jumpRequest = false;
     }
 
     private void CalculateVelocity()
     {
         if (verticalMomentum > gravity)
-            verticalMomentum += Time.fixedDeltaTime * gravity;
+            verticalMomentum += Time.fixedDeltaTime * gravity; //fixedDeltaTime ENSURES A SMOOTH JUMP, gravity HOLDS THE PLAYER BACK A BIT
 
         if (isSprinting)
-            velocity = ((transform.forward * vertical) + (transform.right * horizontal)) * Time.fixedDeltaTime * sprintSpeed;
+            velocity = ((transform.forward * vertical) + (transform.right * horizontal)) * Time.fixedDeltaTime * sprintSpeed; //IF THE PLAYER WANTS TO SPRINT, TAKE THEIR INPUTS, MULTIPLY THEM BY TIME FOR SMOOTHNESS THEN MULTIPLY BY THE sprintSpeed MODIFIER
         else
-            velocity = ((transform.forward * vertical) + (transform.right * horizontal)) * Time.fixedDeltaTime * walkSpeed;
+            velocity = ((transform.forward * vertical) + (transform.right * horizontal)) * Time.fixedDeltaTime * walkSpeed; //IF THE PLAYER WANTS TO WALK, TAKE THEIR INPUTS, MULTIPLY THEM BY TIME FOR SMOOTHNESS THEN MULTIPLY BY THE walkSpeed MODIFIER
 
-        velocity += Vector3.up * verticalMomentum * Time.fixedDeltaTime;
+        velocity += Vector3.up * verticalMomentum * Time.fixedDeltaTime; //ADD ANY UPWARD MOMENTUM TO THE PLAYER'S VECTOR3 POSITION
     }
 
     private void GetPlayerInputs()
     {
-        horizontal = Input.GetAxis("Horizontal");
+        horizontal = Input.GetAxis("Horizontal"); //CHECK ALL THE INPUT AXES FOR VALUES
         vertical = Input.GetAxis("Vertical");
         mouseHorizontal = Input.GetAxis("Mouse X");
         mouseVertical = Input.GetAxis("Mouse Y");
 
-        if (Input.GetButtonDown("Sprint"))
+        if (Input.GetButtonDown("Sprint")) //IF STATEMENTS CHANGE OUR CHECK BOOLEANS
         {
             isSprinting = true;
         }
