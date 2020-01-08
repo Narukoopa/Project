87131aa87f9e186c9df3171de865db1bb1e2ac86
Josh Woodward <58075821+JWoodward230501@users.noreply.github.com>
2020-01-08 21:26:32 +0000

    0.0.5

---
+++ Eros/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+
+    private GameObject player;
+    private NavMeshAgent navAgent;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    
+    void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(player.transform.position, this.transform.position);
+
+        if(distanceToPlayer < 15)
+            navAgent.SetDestination(player.transform.position);
+    }
+}
---
+++ Eros/Assets/Scripts/Player.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool isGrounded;
+    public bool isSprinting;
+    private bool jumpRequest;
+
+    private Transform cam;
+
+    public float walkSpeed = 3f;
+    public float sprintSpeed = 6f;
+    public float jumpForce = 5f;
+    public float gravity = -9.8f;
+    public float playerWidth = 0.15f;
+
+    private float health = 100.0f;
+    private float horizontal;
+    private float vertical;
+    private float mouseHorizontal;
+    private float mouseVertical;
+    private float verticalMomentum = 0;
+
+    private Vector3 velocity;
+
+    private void Start()
+    {
+        cam = Camera.main.transform;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void FixedUpdate()
+    {
+        CalculateVelocity();
+        if (jumpRequest)
+            Jump();
+
+        transform.Rotate(Vector3.up * mouseHorizontal); //PLAYER BODY TURN
+        cam.Rotate(Vector3.right * -mouseVertical); //CAMERA PAN
+        transform.Translate(velocity, Space.World); //MOVE PLAYER IN WORLD
+    }
+
+    private void Update()
+    {
+        GetPlayerInputs();
+    }
+
+    public void DamagePlayer(float value)
+    {
+        health = (health - value);
+    }
+
+    void Jump()
+    {
+        verticalMomentum = jumpForce;
+        isGrounded = false;
+        jumpRequest = false;
+    }
+
+    private void CalculateVelocity()
+    {
+        if (verticalMomentum > gravity)
+            verticalMomentum += Time.fixedDeltaTime * gravity;
+
+        if (isSprinting)
+            velocity = ((transform.forward * vertical) + (transform.right * horizontal)) * Time.fixedDeltaTime * sprintSpeed;
+        else
+            velocity = ((transform.forward * vertical) + (transform.right * horizontal)) * Time.fixedDeltaTime * walkSpeed;
+
+        velocity += Vector3.up * verticalMomentum * Time.fixedDeltaTime;
+    }
+
+    private void GetPlayerInputs()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        mouseHorizontal = Input.GetAxis("Mouse X");
+        mouseVertical = Input.GetAxis("Mouse Y");
+
+        if (Input.GetButtonDown("Sprint"))
+        {
+            isSprinting = true;
+        }
+
+        if (Input.GetButtonUp("Sprint"))
+        {
+            isSprinting = false;
+        }
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            jumpRequest = true;
+        }
+    }
+}
